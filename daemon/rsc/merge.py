ff83d2f97e26368035c0ed8fdb9383994e4e2449
Daehyun You <daehyun.park.you@gmail.com>
2018-07-21 02:07:09 +0900

    Update script daemon.rsc.merge

--- daemon/rsc/merge.py
+++ daemon/rsc/merge.py
@@ -5,7 +5,7 @@ from argparse import ArgumentParser
 from functools import reduce
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.functions import udf, col
-from saclatools import scalars_at, SpkHits
+from saclatools import SpkHits
 
 # %% parser & parameters
 parser = ArgumentParser(prog='merge', description="Merge resorted root files and SACLA meta data.")
@@ -17,14 +17,14 @@ args = parser.parse_args()
 targets = args.rootfiles
 saveas = args.output
 
-hightag = 201704
-equips = {  # must be correct for metadata retrieval
-    'fel_status': ('xfel_mon_ct_bl1_dump_1_beamstatus/summary', bool),
-    'fel_shutter': ('xfel_bl_1_shutter_1_open_valid/status', bool),
-    'laser_shutter': ('xfel_bl_1_lh1_shutter_1_open_valid/status', bool),
-    'fel_intensity': ('xfel_bl_1_tc_gm_1_pd_fitting_peak/voltage', float),
-    'delay_motor': ('xfel_bl_1_st_4_motor_22/position', float)
-}
+# hightag = 201704
+# equips = {  # must be correct for metadata retrieval
+#     'fel_status': ('xfel_mon_ct_bl1_dump_1_beamstatus/summary', bool),
+#     'fel_shutter': ('xfel_bl_1_shutter_1_open_valid/status', bool),
+#     'laser_shutter': ('xfel_bl_1_lh1_shutter_1_open_valid/status', bool),
+#     'fel_intensity': ('xfel_bl_1_tc_gm_1_pd_fitting_peak/voltage', float),
+#     'delay_motor': ('xfel_bl_1_st_4_motor_22/position', float)
+# }
 
 
 # %% initialize spark builder
@@ -35,7 +35,6 @@ builder = (SparkSession
            # .config("spark.executor.cores", 5)
            # .config("spark.executor.memory", "4g")
            )
-spark = builder.getOrCreate()
 
 
 # %%
@@ -48,13 +47,11 @@ def combine_hits(xarr, yarr, tarr, flagarr):
              } for x, y, t, f in zip(xarr, yarr, tarr, flagarr)]
 
 
-chits = col('SortedEvent.fDetektors')[0]['fDetektors_fHits']
-
-
 # %% to parquet
 with builder.getOrCreate() as spark:
     loaded = (spark.read.format("org.dianahep.sparkroot").load(fn) for fn in targets)
     df = reduce(DataFrame.union, loaded)
+    chits = col('SortedEvent.fDetektors')[0]['fDetektors_fHits']
     restructed = (
         df
             .withColumn('hits', combine_hits(chits.getField('fX_mm'),
@@ -63,13 +60,15 @@ with builder.getOrCreate() as spark:
                                              chits.getField('fRekmeth')))
             .select(col('SortedEvent.fEventID').alias("tag"), 'hits')
     )
-    tags = restructed.select('tag').toPandas()['tag']
-    meta = spark.createDataFrame(
-        scalars_at(*tags, hightag=hightag, equips=equips)
-            .rename_axis('tag')
-            .reset_index()
-    )
-    merged = restructed.join(meta, restructed['tag'] == meta['tag'], 'inner')
+    # tags = restructed.select('tag').toPandas()['tag']
+    # if True:
+    # meta = spark.createDataFrame(
+    #     scalars_at(*tags, hightag=hightag, equips=equips)
+    #         .rename_axis('tag')
+    #         .reset_index()
+    # )
+    # merged = restructed.join(meta, restructed['tag'] == meta['tag'], 'inner')
+    merged = restructed
     (
         merged
             .write
