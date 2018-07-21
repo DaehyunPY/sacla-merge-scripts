f789ead25b521104c8d3467772556fa167f3a6ed
Daehyun You <daehyun.park.you@gmail.com>
2018-07-21 14:00:00 +0900

    Merge the data with sacla meta data

--- daemon/rsc/merge.py
+++ daemon/rsc/merge.py
@@ -5,26 +5,27 @@ from argparse import ArgumentParser
 from functools import reduce
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.functions import udf, col
-from saclatools import SpkHits
+from saclatools import SpkHits, scalars_at
 
 # %% parser & parameters
 parser = ArgumentParser(prog='merge', description="Merge resorted root files and SACLA meta data.")
 parser.add_argument('rootfiles', metavar='filenames', type=str, nargs='+',
-                    help='resorted root files to be merge with SACLA meta data')
+                    help='resorted root files to be merged')
 parser.add_argument('-o', '--output', metavar='filename', type=str, default='merged.parquet',
                     help='filename to which the merged data is saved')
 args = parser.parse_args()
-targets = args.rootfiles
+rootfiles = args.rootfiles
 saveas = args.output
 
-# hightag = 201704
-# equips = {  # must be correct for metadata retrieval
-#     'fel_status': ('xfel_mon_ct_bl1_dump_1_beamstatus/summary', bool),
-#     'fel_shutter': ('xfel_bl_1_shutter_1_open_valid/status', bool),
-#     'laser_shutter': ('xfel_bl_1_lh1_shutter_1_open_valid/status', bool),
-#     'fel_intensity': ('xfel_bl_1_tc_gm_1_pd_fitting_peak/voltage', float),
-#     'delay_motor': ('xfel_bl_1_st_4_motor_22/position', float)
-# }
+hightag = 201802
+equips = {  # must be correct for metadata retrieval
+    'fel_status': ('xfel_mon_bpm_bl3_0_3_beamstatus/summary', bool),
+    'fel_shutter': ('xfel_bl_3_shutter_1_open_valid/status', bool),
+    'laser_shutter': ('xfel_bl_3_lh1_shutter_1_open_valid/status', bool),
+    'delay_motor_st4': ('xfel_bl_3_st_4_motor_25/position', float),
+    'delay_motor_st1': ('xfel_bl_3_st_1_motor_73/position', float),
+}
+tmafiles = ["/work/uedalab/2018A8038Ueda/tma/*.csv"]
 
 
 # %% initialize spark builder
@@ -49,7 +50,7 @@ def combine_hits(xarr, yarr, tarr, flagarr):
 
 # %% to parquet
 with builder.getOrCreate() as spark:
-    loaded = (spark.read.format("org.dianahep.sparkroot").load(fn) for fn in targets)
+    loaded = (spark.read.format("org.dianahep.sparkroot").load(fn) for fn in rootfiles)
     df = reduce(DataFrame.union, loaded)
     chits = col('SortedEvent.fDetektors')[0]['fDetektors_fHits']
     restructed = (
@@ -60,19 +61,30 @@ with builder.getOrCreate() as spark:
                                              chits.getField('fRekmeth')))
             .select(col('SortedEvent.fEventID').alias("tag"), 'hits')
     )
-    # tags = restructed.select('tag').toPandas()['tag']
-    # if True:
-    # meta = spark.createDataFrame(
-    #     scalars_at(*tags, hightag=hightag, equips=equips)
-    #         .rename_axis('tag')
-    #         .reset_index()
-    # )
-    # merged = restructed.join(meta, restructed['tag'] == meta['tag'], 'inner')
-    merged = restructed
-    (
-        merged
-            .write
-            .option("maxRecordsPerFile", 10000)  # less than 10 MB assuming a record of 1 KB,
-            .mode('overwrite')
-            .parquet(saveas)
+    tma = (
+        spark
+            .read
+            .csv(*tmafiles, header=True, inferSchema=True, comment='[')
+            .select(col('tag_number').alias('tag'), col('timing_edge_fitting(pixel)').alias('tma_edge'))
     )
+    tags = restructed.select('tag').toPandas()['tag']
+    lasttag, *_ = tma.selectExpr('MAX(tag) AS lasttag').toPandas()['lasttag'].values
+
+    if tags.max() <= lasttag:
+        meta = spark.createDataFrame(
+            scalars_at(*tags, hightag=hightag, equips=equips)
+                .rename_axis('tag')
+                .reset_index()
+        )
+        merged = (
+            restructed
+                .join(meta, "tag", 'inner')
+                .join(tma, "tag", 'inner')
+        )
+        (
+            merged
+                .write
+                .option("maxRecordsPerFile", 100000)  # less than 10 MB assuming a record of 1 KB,
+                .mode('overwrite')
+                .parquet(saveas)
+        )
